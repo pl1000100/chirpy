07f54b3ccfbb68e45e7744907828347b1cf83a90
pl1000100 <olszynski97@gmail.com>
2025-03-04 19:33:46 +0100

    Ready server

--- main.go
+++ main.go
@@ -9,11 +9,18 @@ func main() {
 	const filePathRoot = "."
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filePathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))))
+	mux.HandleFunc("/healthz", handlerReadiness)
 
 	svr := &http.Server{
 		Handler: mux,
 		Addr:    ":" + port,
 	}
 	svr.ListenAndServe()
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
